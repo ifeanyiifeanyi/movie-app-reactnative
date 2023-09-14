df02f9464c8687316050a0710f72efd8c33b5fe5
Ifeanyi Nnaemego <josephnaemego@gmail.com>
2023-09-14 11:21:02 +0100

    new

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -14,35 +14,36 @@ const Splash = () => {
   const [isConnected, setIsConnected] = useState(true);
 
 
-  useEffect(() => {
-    NetInfo.addEventListener(state => {
-      console.log('Connection type: ', state.type);
-      console.log('Is connected? ', state.isConnected);
-      console.log('Is internet reachable? ', state.isInternetReachable);
-    });
-    NetInfo.fetch().then(state => {
-      setIsConnected(state.isConnected);
-    });
-    return () => {
-      console.log("calling network error");
-    };
+  // useEffect(() => {
+  //   NetInfo.addEventListener(state => {
+  //     console.log('Connection type: ', state.type);
+  //     console.log('Is connected? ', state.isConnected);
+  //     console.log('Is internet reachable? ', state.isInternetReachable);
+  //   });
+  //   NetInfo.fetch().then(state => {
+  //     setIsConnected(state.isConnected);
+  //   });
+  //   return () => {
+  //     console.log("calling network error");
+  //   };
     
-  }, []);
+  // }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      if (isConnected) {
-        handleGetToken();
-      } else {
-        Alert.alert("Connection Failed!", [
-          {
-            text: "Try Again",
-            onPress: () => DevSettings.reload(),
-            style: "cancel"
-          }
-        ]);
-      }
-    }, 2000);
+    // setTimeout(() => {
+    //   if (isConnected) {
+    //     handleGetToken();
+    //   } else {
+    //     Alert.alert("Connection Failed!", [
+    //       {
+    //         text: "Try Again",
+    //         onPress: () => DevSettings.reload(),
+    //         style: "cancel"
+    //       }
+    //     ]);
+    //   }
+    // }, 2000);
+    handleGetToken();
   }, [])
   const handleGetToken = async () => {
     const dataToken = await AsyncStorage.getItem('name');
@@ -55,7 +56,7 @@ const Splash = () => {
 
   return (
     <View style={myStyle.container}>
-      <Image source={require('../img/logo/loading1.gif')} style={{ width: 200, height: 200 }} />
+      <Image source={require('../../assets/1adaptive-icon.png')} style={{ width: 200, height: 200 }} />
     </View>
   )
 }
